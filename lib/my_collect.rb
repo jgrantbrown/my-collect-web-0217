f81704f4239bdf85154a8871b41a65260cf50ee5
jason brown <j.grant.brown@gmail.com>
2017-01-07 04:01:25 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  array
+end
 
+my_collect(students) do |student|
+  student.split(" ").first
+end
 
+my_collect(languages) do |language|
+      language.uppercase
+    end
